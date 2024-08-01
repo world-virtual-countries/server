870ff092bda9d35ba4dde460fc803ea98c13aa4e
piwerm <m.buldakov10min@gmail.com>
2024-08-02 00:26:52 +0200

    Testing

--- routers/callback.py
+++ routers/callback.py
@@ -11,10 +11,19 @@ async def callback_handler(request: Request, background_task: BackgroundTasks):
     try:
         data = await request.json()
         
+        logger.critical("START")
+        
         if data["type"] == "confirmation":
+            logger.critical("CONFIRMATION")
             return CALLBACK_CONFIRMATION
         if data["secret"] == CALLBACK_SECRET:
+            logger.critical("SECRET")
+            logger.critical(data["secret"])
+            logger.critical(bot.callback.get_secret_key())
+            logger.critical(CALLBACK_SECRET)
             background_task.add_task(bot.process_event, data)
+            
+        logger.critical("END")
             
         return "ok"
     except Exception as e:
