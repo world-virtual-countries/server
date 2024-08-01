7224b19f0282e60d428fef0873549cec7295fe88
piwerm <m.buldakov10min@gmail.com>
2024-08-02 00:05:50 +0200

    Update api.py

--- api.py
+++ api.py
@@ -7,10 +7,6 @@ app = FastAPI(default_response_class=OrderedJSONResponse)
 app.router.include_router(routers.main.router)
 app.router.include_router(routers.callback.router)
 
-@app.on_event("startup")
-async def startup_event():
-    await bot.setup_webhook()
-
 @app.exception_handler(StarletteHTTPException)
 async def exception_handler(request: Request, exception: StarletteHTTPException):
     return OrderedJSONResponse(
